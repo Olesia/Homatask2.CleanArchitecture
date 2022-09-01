bda57bf8637d7b5922d9fd034f3ee76baac57001
Olesia Skrypchenko <Olesia_Skrypchenko@epam.com>
2022-09-01 19:15:26 +0200

    fix: supress ItemDto Name warning

--- src/Application/Items/Queries/ItemDto.cs
+++ src/Application/Items/Queries/ItemDto.cs
@@ -4,19 +4,11 @@ using Homatask2.CleanArchitecture.Domain.Entities;
 namespace Homatask2.CleanArchitecture.Application.Items.Queries;
 
 public class ItemDto : IMapFrom<Item>
-{
-    public ItemDto(string name, string? description, string? image, decimal price, uint amount, int categoryId)
-    {
-        Name = name;
-        Description = description;
-        Image = image;
-        Price = price;
-        Amount = amount;
-        CategoryId = categoryId;
-    }
-
+{ 
     public int Id { get; set; }
+    #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public string Name { get; set; }
+    #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public string? Description { get; set; }
     public string? Image { get; set; }
 
